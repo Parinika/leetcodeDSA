2d81f73891311b92439cd5013edf05f0fe0e0835
Parinika Jain <90214795+Parinika@users.noreply.github.com>
2022-06-20 21:24:37 +0530

    Time: 0 ms (100.00%), Space: 5.7 MB (96.32%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,29 +1,44 @@
 class Solution {
 public:
-    int store[31];
-    int fibonacci(int no){
-        if(no<=1)
-            return no;
-        //cheching if we have value of these numbers already stored
+    // recursive way
+//     int store[31];
+//     int fibonacci(int no){
+//         if(no<=1)
+//             return no;
+//         //cheching if we have value of these numbers already stored
         
-        if(store[no]!=-1){
-            return store[no];
-        }
+//         if(store[no]!=-1){
+//             return store[no];
+//         }
         
-        int value1=fibonacci(no-1);
-        int value2=fibonacci(no-2);
+//         int value1=fibonacci(no-1);
+//         int value2=fibonacci(no-2);
         
-        int ans=value1+value2;
+//         int ans=value1+value2;
         
-        store[no]=ans; // storing the value also called memoizing the solutions        
-        return ans;
+//         store[no]=ans; // storing the value also called memoizing the solutions        
+//         return ans;
+//     }
+    
+//     int fib(int n) {
+        
+//         //initialize the values of an array we use memset()
+        
+//         memset(store,-1,sizeof(store));
+//         return fibonacci(n);
+//     }
+    
+    //iterative way
+    int fib(int n){
+        
+        int fibonacci[35] = {};
+        fibonacci[0]=0;
+        fibonacci[1]=1;
+        
+        for(int i=2;i<=n;i++){
+            fibonacci[i]=fibonacci[i-1]+fibonacci[i-2];
+            }
+        return fibonacci[n];
     }
     
-    int fib(int n) {
-        
-        //initialize the values of an array we use memset()
-        
-        memset(store,-1,sizeof(store));
-        return fibonacci(n);
-    }
 };
