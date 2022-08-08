54e3ab8bbc9b276555c1bed978f12b69722248eb
Parinika Jain <90214795+Parinika@users.noreply.github.com>
2022-08-09 03:55:30 +0530

    Time: 5 ms (69.73%), Space: 6 MB (77.71%) - LeetHub

--- 52-n-queens-ii/52-n-queens-ii.cpp
+++ 52-n-queens-ii/52-n-queens-ii.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     int count=0;
-    void solve(int col,int n,vector<int> &leftrow,vector<int> &upperdiagonal, vector<int> &lowerdiagonal, vector<string> &board){
+    void solve(int col,int n,vector<int> &leftrow,vector<int> &upperdiagonal, vector<int> &lowerdiagonal){
         if(col==n){
             count++;
             return;
         }
         for(int row=0;row<n;row++){
             if(leftrow[row]==0 and upperdiagonal[n-1+col-row]==0 and lowerdiagonal[col+row]==0){             
-                board[row][col]='Q';
+                // board[row][col]='Q';
                 leftrow[row]=1;
                 upperdiagonal[n-1+col-row]=1;
                 lowerdiagonal[row+col]=1;
-                solve(col+1,n,leftrow,upperdiagonal,lowerdiagonal,board);
-                board[row][col]='.';
+                solve(col+1,n,leftrow,upperdiagonal,lowerdiagonal);
+                // board[row][col]='.';
                 leftrow[row]=0;
                 upperdiagonal[n-1+col-row]=0;
                 lowerdiagonal[row+col]=0;
@@ -22,14 +22,14 @@ public:
     }
     int totalNQueens(int n) {
         // int count=0;
-        vector<string> board(n);
-        string s(n,'.');
-        for(int i=0;i<n;i++)
-            board[i]=s;
+        // vector<string> board(n);
+        // string s(n,'.');
+        // for(int i=0;i<n;i++)
+        //     board[i]=s;
         vector<int> leftrow(n,0);
         vector<int> upperdiagonal(2*n-1,0);
         vector<int> lowerdiagonal(2*n-1,0);
-        solve(0,n,leftrow,upperdiagonal,lowerdiagonal,board);
+        solve(0,n,leftrow,upperdiagonal,lowerdiagonal);
         return count;
     }
 };
